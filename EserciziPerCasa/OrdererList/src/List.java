35ed36b51e49acf2b434bd97044c7ee58eeb3f9c
riccardo.tonini <romolo.romulus.771@gmail.com>
2025-10-22 11:02:16 +0200

    FInished es1

--- EserciziPerCasa/OrdererList/src/List.java
+++ EserciziPerCasa/OrdererList/src/List.java
@@ -11,14 +11,14 @@ public class List<T> {
             head = nuovo;
         }
         else if (head.getNext() == null) {
-
+            head.setNext(nuovo);
         }
         else{
             Nodo<T> current = head;
 
             while (current != null){
-                if (nuovo.getVal() > current.getVal() && current.getNext().getVal() != null) {
-                    if(nuovo.getVal() < current.getNext().getVal()){
+                if (nuovo.getVal() > current.getVal() && current.getNext().getVal() != null) {      //Se il valore nuovo è maggiore del corrente
+                    if(nuovo.getVal() < current.getNext().getVal()){                                //se il valore è compreso tra corrente e il prossimo di corrente
                         current.getNext().setPrev(nuovo);
                         nuovo.setNext(current.getNext());
                         nuovo.setPrev(current);
