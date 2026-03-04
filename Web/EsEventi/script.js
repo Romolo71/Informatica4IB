d0d7febe7698c2df27eec69dd6b809caaefe01d1
Tonini Riccardo <riccardo.tonini@cz.lan>
2026-03-04 11:51:16 +0100

    Added esEventi

---
+++ Web/EsEventi/script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.querySelector("#nep").addEventListener("click", function(){
+        alert("2,7182818284");
+    })
+
+    document.querySelector("#grav").addEventListener("click", function(){
+        alert("6,674 X 10^-11");
+    })
+
+    document.querySelector("#luce").addEventListener("click", function(){
+        alert("299792458 m/s");
+    })
+
+    document.querySelector("#planck").addEventListener("click", function(){
+        alert("6,62618·10^-34 J·s");
+    })
+
+    document.querySelector("#k").addEventListener("click", function(){
+        alert("n");
+    })
+})
